7cab14cb86bfde0e40769a85fb9033752f425337
nikki <nicolezoopzoop@gmail.com>
2016-08-08 17:47:32 -0500

    First commit

---
+++ Endless_RPG.py
@@ -0,0 +1,32 @@
+#Endless RPG:
+'''
+Endlessly looping RPG!
+
+Todo:
+    * Battle function
+        - while loop
+        - turn decider
+        - combatant alive check
+        - character action options:
+            - attack:
+                Deals predictable amt of damage. Low risk.
+
+            - block:
+                Cuts opponent dmg reliably.
+
+            - risky attack:
+                Roll for a chance to double your attack, but risk delivering a weak attack if it fails.
+            
+            - wind-up attack:
+                Leaves character vunerable for current turn, but attack damage gaurunteed to be double on next turn
+
+    * Character object
+        - hp
+        - atk
+        - alive= true/false
+        
+        - Player(Character)
+            - Decides on turn whether to attack, risky attack, block, or wind-up attack
+
+    
+'''
