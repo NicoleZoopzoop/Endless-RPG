49dd8404ad93dcc6db921b1b9a2ce3953abc1ddd
nikki <nicolezoopzoop@gmail.com>
2016-08-08 21:56:11 -0500

    Initial code

--- Endless_RPG.py
+++ Endless_RPG.py
@@ -1,32 +1,73 @@
 #Endless RPG:
 '''
-Endlessly looping RPG!
-
-Todo:
-    * Battle function
-        - while loop
-        - turn decider
-        - combatant alive check
-        - character action options:
-            - attack:
-                Deals predictable amt of damage. Low risk.
-
-            - block:
-                Cuts opponent dmg reliably.
-
-            - risky attack:
-                Roll for a chance to double your attack, but risk delivering a weak attack if it fails.
-            
-            - wind-up attack:
-                Leaves character vunerable for current turn, but attack damage gaurunteed to be double on next turn
-
-    * Character object
-        - hp
-        - atk
-        - alive= true/false
-        
-        - Player(Character)
-            - Decides on turn whether to attack, risky attack, block, or wind-up attack
-
-    
+   
 '''
+from random import randint
+
+#Character objects are the combatants taking turns in the battle loop
+class Character:
+    def __init__(self, name, hp, atk):
+        self.name = name
+        self.hp = hp
+        self.atk = atk
+        self.alive = True
+        self.blocking = False
+        self.winding_up = False
+
+    #damage handling, also checks if character taking damage is blocking
+    def take_damage(self, damage):
+        if self.blocking:        
+            self.hp -= int(damage / 3)
+            self.blocking = False        
+
+        else:
+            self.hp -= damage
+    
+    #character action options
+
+    def attack(self):
+        return randint((int(self.atk / 2)), self.atk)
+
+
+    def risky_attack(self):
+        if randint(1, 5) == 5:
+            print("Risky attack suceeds!\n")
+            return self.atk*2
+    
+        else:  
+            print("Risky attack fails!\n")
+            return int(self.atk / 10)
+
+    def windup_attack(self, target):
+        if self.winding_up == True:   
+            print("A powerful blow!")
+            self.winding_up = False
+            return self.atk * 2
+        
+        else:
+            self.winding_up = True
+            print("Gathering strength...\n")
+
+
+#Characters are either Players, or Enemys
+
+#this is a placeholder Enemy
+class Enemy(Character):
+    def __init__(self, Player):
+        super(Enemy, self).__init__()
+        self.name = 'monster'
+        self.hp = randint(1, player.hp)
+        self.atk = randint(1, player.atk)
+
+#the Player
+class Player(Character):
+    def __init__(self):
+        super(Character, self).__init__()
+        self.name = 'you'
+        self.hp = 10
+        self.atk = 290
+        
+
+#tests
+
+
